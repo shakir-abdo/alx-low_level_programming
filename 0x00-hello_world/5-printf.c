885e82cf94eaeca5cb3b675686f73f6705fe0a0a
Shakir Abdo <shicolare1@gmail.com>
2023-08-10 03:28:17 -0700

    add hello,print c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print the text with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if exited with no errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
