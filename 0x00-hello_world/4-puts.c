aa0bc1da21705ce8e4b154038e45c2125738108b
Shakir Abdo <shicolare1@gmail.com>
2023-08-10 03:19:39 -0700

    fix hello,put c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ *  print the text "Programming is like building a multilingual puzzle"
+ *  and return 0 if exexuted with no errors
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
