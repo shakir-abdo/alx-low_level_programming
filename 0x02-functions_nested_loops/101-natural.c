d3d8cc446cb919d52f63a8d75347524687c518fb
Shakir Abdo <shicolare1@gmail.com>
2023-08-15 11:47:41 -0700

    fix 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,25 +7,24 @@
  */
 int main(void)
 {
-	unsigned long int sum1, sum2, sum;
+	unsigned long int sum3, sum5, sum;
 	int i;
 
-	sum1 = 0;
-	sum2 = 0;
+	sum3 = 0;
+	sum5 = 0;
 	sum = 0;
 
 	for (i = 0; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
 		{
-			sum1 = sum1 + i;
+			sum3 = sum3 + i;
 		} else if ((i % 5) == 0)
 		{
-			sum2 = sum2 + i;
+			sum5 = sum5 + i;
 		}
 	}
-	sum = sum1 + sum2;
+	sum = sum3 + sum5;
 	printf("%lu\n", sum);
 	return (0);
 }
-
