203047194244016e319adc745e03742be2cb339d
Shakir Abdo <shicolare1@gmail.com>
2023-09-28 15:07:13 -0700

    add 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - convert a binary number
+ * to unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len;
+	unsigned int total, power;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
+}
