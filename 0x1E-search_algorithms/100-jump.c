79eab1badd53ae9e2cdfd14e920a7c98e6490978
Shakir Abdo <shicolare1@gmail.com>
2024-03-13 19:53:51 +0300

    fix task 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @value: value to search in
+ * @array: input array
+ * @size: size of the array
+ * Return: index of the number
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int i, m, k, prev;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = i = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+		k++;
+		prev = i;
+		i = k * m;
+	} while (i < (int)size && array[i] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, i);
+
+	for (; prev <= i && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
+
+	return (-1);
+}
